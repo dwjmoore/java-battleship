101265285ec83e0d2fa8aa6ac83c1d8f1cb87cbc
David Moore <dwjmoore@gmail.com>
2023-04-13 22:26:22 +0000

    made printBoard public; added getter methods for player and enemy boards

--- Board.java
+++ Board.java
@@ -8,14 +8,25 @@ public class Board {
 
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
-				playerBoard[i][j] = "";
-				enemyBoard[i][j] = "";
+				playerBoard[i][j] = "X";
+				enemyBoard[i][j] = "X";
 			}
 		}
 	}
 
-	private void printBoard() {
-		System.out.println("Print board function called, but not implemented.");
+	public void printBoard(String[][] board) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				System.out.println(board[i][j]);
+			}
+		}
 	}
 
+	public String[][] getPlayerBoard() {
+		return playerBoard;
+	}
+
+	public String[][] getEnemyBoard() {
+		return enemyBoard;
+	}
 }
